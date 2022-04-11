9bb35586b7afaafa35c9f74d14152679b24eec69
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-04-11 23:15:58 +0300

    init

---
+++ scripts/index.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// HOMEWORK #11
+
+let yearOfBirth = +prompt("Введите год Вашего рождения");
+
+if (
+  yearOfBirth > 2020 ||
+  yearOfBirth < 1920 ||
+  isNaN(yearOfBirth) ||
+  yearOfBirth.length < 4
+) {
+  alert("Жаль, что Вы не захотели ввести год Вашего рождения");
+}
+
+let city = prompt("Введите город Вашего проживания");
+
+switch (city) {
+  case "Киев":
+  case "киев":
+  case "Київ":
+    city = "Ты живешь в столице Украины";
+    break;
+  case "Москва":
+  case "москва":
+    city = "Русский военный корабль, иди на*уй";
+    break;
+  case "Минск":
+  case "минск":
+    city = "Ты живешь в столице Белоруссии";
+    break;
+
+  default:
+    city = `Ты живешь в городе ${city}`;
+}
+
+let sport = prompt("Введите Ваш любимый вид спорта");
+
+switch (sport) {
+  case "баскетбол":
+  case "Баскетбол":
+    sport = "Круто! Хочешь стать Майклом Джорданом?";
+    break;
+
+  case "футбол":
+  case "Футбол":
+    sport = "Круто! Хочешь стать Пеле?";
+    break;
+
+  case "бокс":
+  case "Бокс":
+    sport = "Круто! Хочешь стать Майклом Тайсоном?";
+    break;
+
+  default:
+    city = `Круто, успехов в таком деле, как ${city}`;
+}
+
+alert(`${yearOfBirth} \n ${city} \n ${sport}`);
