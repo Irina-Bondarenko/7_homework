1b9f7d711d4525335b572b24ff1d00a02b7159bf
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-04-12 22:02:00 +0300

    init

--- scripts/index.js
+++ scripts/index.js
@@ -4,13 +4,10 @@
 
 let yearOfBirth = +prompt("Введите год Вашего рождения");
 
-if (
-  yearOfBirth > 2020 ||
-  yearOfBirth < 1920 ||
-  isNaN(yearOfBirth) ||
-  yearOfBirth.length < 4
+if (yearOfBirth > 2020 || yearOfBirth < 1920 || isNaN(yearOfBirth) || yearOfBirth.length < 4
 ) {
   alert("Жаль, что Вы не захотели ввести год Вашего рождения");
+  yearOfBirth = "Нет данных";
 }
 
 let city = prompt("Введите город Вашего проживания");
@@ -28,6 +25,11 @@ switch (city) {
   case "Минск":
   case "минск":
     city = "Ты живешь в столице Белоруссии";
+    break;
+
+  case null:
+    alert("Жаль, что Вы не захотели ввести город Вашего проживания");
+    city = "Нет данных";
     break;
 
   default:
@@ -52,8 +54,17 @@ switch (sport) {
     sport = "Круто! Хочешь стать Майклом Тайсоном?";
     break;
 
+  case null:
+    alert("Жаль, что Вы не захотели ввести Ваш любимый вид спорта");
+    sport = "Нет данных";
+    break;
+
   default:
     sport = `Круто, успехов в таком спорте, как ${sport}`;
 }
-
-alert(`${yearOfBirth} \n ${city} \n ${sport}`);
+if (yearOfBirth === "Нет данных" && city === "Нет данных" && sport === "Нет данных") {
+alert("Жаль, но Вы не ввели никаких данных");
+}
+else {
+  alert(`${yearOfBirth} \n ${city} \n ${sport}`)
+}
